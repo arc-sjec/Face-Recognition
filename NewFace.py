ca0e58bf20b4f04f7bf223d140eae514c8289b24
Automation and Robotics Club <109940879+arc-sjec@users.noreply.github.com>
2022-09-01 19:27:19 +0530

    Add files via upload

---
+++ NewFace.py
@@ -0,0 +1,196 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import cv2
+#import face_recognition
+import pickle
+import numpy
+from PIL import ImageQt
+import os
+
+Image = None
+
+
+class Ui_NewFaceWindow(object):
+    def Main(self, MainWindow):
+        from mainScreen import Ui_MainWindow
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.window)
+        self.window.show()
+        self.facerec_window.hide()
+
+    def setupUi(self, NewFaceWindow, MainWindow):
+        self.new_face_window = NewFaceWindow
+        self.main_window = MainWindow
+        self.facerec_window = NewFaceWindow
+        NewFaceWindow.setObjectName("NewFaceWindow")
+        NewFaceWindow.resize(800, 800)
+        self.centralwidget = QtWidgets.QWidget(NewFaceWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, -30, 1161, 821))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setAlignment(QtCore.Qt.AlignCenter)
+        self.Feedlabel = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.Feedlabel.setObjectName("label")
+        self.verticalLayout.addWidget(self.Feedlabel)
+        self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+
+
+        self.exitButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.exitButton.setObjectName("exitButton")
+        self.verticalLayout.addWidget(self.exitButton)
+
+        NewFaceWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(NewFaceWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1159, 26))
+        self.menubar.setObjectName("menubar")
+        NewFaceWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(NewFaceWindow)
+        self.statusbar.setObjectName("statusbar")
+        NewFaceWindow.setStatusBar(self.statusbar)
+
+        self.Worker1 = Worker1()
+        self.Worker1.start()
+
+        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+
+
+        self.exitButton.clicked.connect(self.CancelFeed)
+        self.pushButton.clicked.connect(self.Capture)
+
+        self.facerec_window.setStyleSheet("background-color: #191919; color: white;")
+        self.exitButton.setStyleSheet("color: white; border: 1px solid white;")
+        self.pushButton.setStyleSheet("color: white; border: 1px solid white;")
+
+
+        self.retranslateUi(NewFaceWindow)
+        QtCore.QMetaObject.connectSlotsByName(NewFaceWindow)
+
+    def retranslateUi(self, NewFaceWindow):
+        _translate = QtCore.QCoreApplication.translate
+        NewFaceWindow.setWindowTitle(_translate("NewFaceWindow", "NewFaceWindow"))
+        self.Feedlabel.setText(_translate("NewFaceWindow", "Starting..."))
+        self.pushButton.setText(_translate("NewFaceWindow", "Capture"))
+        self.exitButton.setText(_translate("NewFaceWindow", "Exit"))
+
+    
+    def ImageUpdateSlot(self, Image):
+        self.Feedlabel.setPixmap(QPixmap.fromImage(Image))
+    
+
+    def CancelFeed(self):
+        self.Worker1.stop() 
+        self.Main(self.main_window)
+
+    def Capture(self):
+        self.Worker1.stop()
+        img_name, done = QtWidgets.QInputDialog.getText(self.facerec_window, 'Enter name', 'Enter your name:')
+
+
+        if done:
+            usr_names = []
+
+            image = ImageQt.fromqimage(self.Feedlabel.pixmap().toImage())
+            image.save('train.jpg')
+
+            image = face_recognition.load_image_file("train.jpg")
+            face_encoding = []
+
+            try:
+                face_encoding = face_recognition.face_encodings(image)[0]
+                try:
+                    with open('usr_names.pt', 'rb') as f:
+                        usr_names = pickle.load(f)
+                except:
+                    pass
+
+                usr_names = usr_names + [img_name]
+
+                with open('usr_names.pt', 'wb') as f:
+                    pickle.dump(usr_names, f)
+
+                known_faces = []
+                
+                try:
+                    with open('saved_embeds.pt', 'rb') as f:
+                        known_faces = pickle.load(f)
+                except:
+                    pass
+                
+                known_faces = known_faces + [face_encoding]
+
+                with open('saved_embeds.pt', 'wb') as f:
+                    pickle.dump(known_faces, f)
+
+                os.remove('train.jpg')
+                self.Main(self.main_window)
+            except:
+                print("Face not detected")
+                os.remove('train.jpg')
+                msg = QtWidgets.QMessageBox()
+                msg.setIcon(QtWidgets.QMessageBox.Critical)
+                msg.setText("Face not detected, Try again")
+                msg.setWindowTitle("Face not detected")
+                msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+
+                msg.exec_()
+                self.Main(self.main_window)
+        else:
+            self.Main(self.main_window)
+
+  
+
+
+
+class Worker1(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+
+    def run(self):
+        self.ThreadActive = True
+        Capture = cv2.VideoCapture(0)
+        img = None
+        count = 0
+        name_list =[]
+        while self.ThreadActive:
+            ret, frame = Capture.read()
+            if ret:
+                # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                # Experimental
+                        
+                detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+                rects = detector.detectMultiScale(frame, scaleFactor=1.05, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+                for (x, y, w, h) in rects:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # End
+                
+                ConvertToQtFormat = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+                pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.ImageUpdate.emit(pic)
+
+                
+
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    NewFaceWindow = QtWidgets.QMainWindow()
+    ui = Ui_NewFaceWindow()
+    ui.setupUi(NewFaceWindow,QtWidgets.QMainWindow())
+    NewFaceWindow.show()
+    sys.exit(app.exec_())
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# LIFI
+# FaceRec
+*Face recognition software having the capacity to add new users, as well as the ability to learn a face from only one image.*
+
+### Installation instructions
+###### Step 1: Clone repository
+`git clone https://github.com/Joyel1441/FaceRec`
+###### Step 2: Install software (python 3.6 required)
+`python -r requirements.txt`
+###### Step 3: Run software
+`python mainScreen.py`
+
+### Documentation:
+**FaceRec3.py**: *contains code which can detect faces and recognize them* \
+**NewFace.py**: *contains code which can add a new face for recognition* \
+**mainScreen.py**: *main file which calls FaceRec3 or NewFace* \
+Default username and password to add a new face: \
+username: admin \
+password: admin \
+Main libraries used for face detection are dlib face-recognition library and open-cv
+
+### Screenshots
+#### Main Screen
+![Main Screen](https://raw.githubusercontent.com/Joyel1441/FaceRec/463a3e6bba5d32d0492e552c55439da496aa5fc0/main_screen.png?raw=true "Main Screen")
+#### Adding new face
+![New Face Screen](https://github.com/Joyel1441/FaceRec/blob/9c9cf81ae0c7c587ffde68ad1e9787377894c4bd/newface.png?raw=true "New Face Screen")
+#### Face recognition
+![Recognition Screen](https://github.com/Joyel1441/FaceRec/blob/9c9cf81ae0c7c587ffde68ad1e9787377894c4bd/detection.png?raw=true "Recognition Screen")
+
+If you have any ideas or improvements to be implemented, feel free to contribute
